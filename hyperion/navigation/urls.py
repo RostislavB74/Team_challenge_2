59549cd89a38ac4a0ab085c6c8cf2acb0a911fd1
Rostyslav Bysko <brn.h74@gmail.com>
2025-07-30 06:37:43 +0300

    add caliber

--- hyperion/navigation/urls.py
+++ hyperion/navigation/urls.py
@@ -13,7 +13,6 @@ from .views import menu_view
 
 urlpatterns = [
     path('', menu_view, name='menu_home'),
-    # path('dovidniki/', dovidniki_view, name='dovidniki'),
-    # path('dovidniki/', dovidniki_home, name='dovidniki'),
+
 ]
 app_name = 'navigation'
--- hyperion/tiles/urls.py
+++ hyperion/tiles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import tile_list, caliber_tile_list
-
+from . import views
+from .views import TileListView, CaliberTileListView
 urlpatterns = [
-    path('', tile_list, name='tile_list'),
-    path('caliber/', caliber_tile_list, name='caliber_tile_list'),
+     path("designs/", views.TileListView, name="design_list"),
+    path("caliber/", views.CaliberTileListView, name="caliber_list"),
+    # path('designs/', tile_list, name='tile_list'),
+    # path('caliber/', caliber_tile_list, name='caliber_tile_list'),
 ]
+app_name = 'tiles'
